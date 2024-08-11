b9ed60bbb532ed521caca6bfcefb64870a6c2256
Eric Vicenti <eric@vicenti.net>
2024-08-11 21:21:31 +0200

    Try to fix build

--- dashboard/pages/_app.tsx
+++ dashboard/pages/_app.tsx
@@ -1,20 +1,19 @@
 /**
  * Setup instructions: https://tamagui.dev/docs/guides/next-js#pages_apptsx
  */
-import '@tamagui/core/reset.css'
-import '@tamagui/font-inter/css/400.css'
-import '@tamagui/font-inter/css/700.css'
+import '@tamagui/core/reset.css';
+import '@tamagui/font-inter/css/400.css';
+import '@tamagui/font-inter/css/700.css';
 
-import Head from 'next/head'
-import React from 'react'
-import type { SolitoAppProps } from 'solito'
-import { Provider } from 'src/Provider'
+import Head from 'next/head';
+import React from 'react';
+import { Provider } from 'src/Provider';
 
 if (process.env.NODE_ENV === 'production') {
-  require('../public/tamagui.css')
+  require('../public/tamagui.css');
 }
 
-function App({ Component, pageProps }: SolitoAppProps) {
+function App({ Component, pageProps }) {
   return (
     <>
       <Head>
@@ -25,7 +24,7 @@ function App({ Component, pageProps }: SolitoAppProps) {
         <Component {...pageProps} />
       </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
