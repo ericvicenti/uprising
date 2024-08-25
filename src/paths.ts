e308548a3564a7f0bb5cad5acb7090bb4cbd922b
Eric Vicenti <eric@vicenti.net>
2024-08-25 03:33:19 +0200

    Update paths for new data dir location

--- src/paths.ts
+++ src/paths.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 
-export const statePath = process.env.DATA_PATH || join(__dirname, './../data');
+export const statePath = process.env.DATA_PATH || join(__dirname, './../../Uprising-Data');
+console.log('DATA_PATH:', statePath);
 export const mainStatePath = join(statePath, 'main.json');
 export const libraryPath = join(statePath, 'library');
 export const mediaPath = join(statePath, 'media');
